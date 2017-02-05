902e164f7723054123554796c5b38398ab560688
shukun-zhao <978029437@qq.com>
2017-02-05 16:47:43 +0800

    删除无用文件

--- src/main/webapp/assets/scripts/footer.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	var h='<div class="footer-copyright">'
-		+'<div class="container center">'
-		+'Copyright © 2016 By <a class="grey-text text-lighten-4" href="http://www.cnblogs.com/zhaosk/" target="_blank">zhaosk</a> All rights reserved.'
-		+'</div>'
-		+'</div>';
-	$("#page-footer").html(h);
-});
